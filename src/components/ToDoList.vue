057d68f9c38741030990242cca3fe1c067e92570
Rio <geomilevinc@gmail.com>
2020-01-14 11:38:30 +0200

    Modified ToDoList Component

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,11 +1,63 @@
 <template>
     <div>
-        Todo List goes here
+        <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
+
+        <div v-for="todo in todos" :key="todo.id" class="todo-item">{{ todo.title }}</div>
     </div>
 </template>
 
 <script>
 export default {
     name: "todo-list",
+    data() {
+        return {
+            newTodo: '',
+            idForTodo: 3,
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Finish Vue Screencast',
+                    'completed': false
+                },
+                 {
+                    'id': 2,
+                    'title': 'Take over world',
+                    'completed': false
+                }
+            ]
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodo.trim().length == 0) {
+                return;
+            }
+
+            this.todos.push({
+                id: this.idForTodo,
+                title: this.newTodo,
+                completed: false
+            });
+            this.newTodo = '';
+            this.idForTodo++;        }
+    }
 }
 </script>
+
+<style scoped>
+ div {
+     max-width: 800px;
+     margin: 1em auto;
+ }
+
+ .todo-input {
+     width: 100%;
+     padding: 10px 18px;
+     font-size: 18px;
+     margin-bottom: 16px;
+ }
+
+ .todo-input:focus {
+     outline: 0;
+ }
+</style>
